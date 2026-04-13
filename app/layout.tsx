8ad0ffac5e7fb0d2b05238cd737e768380e1fc81
DevLawer <seathieldev@gmail.com>
2026-04-13 15:33:48 +0200

    Added ui for the login page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NotificationProvider from "@/components/ui/Notification/NotificationProvider";
+import NotificationContainer from "@/components/ui/Notification/NotificationContainer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,14 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <div id="notification-root"></div>
+        <NotificationProvider> 
+            {children}
+           <NotificationContainer/>
+        </NotificationProvider>
+       
+      </body>
     </html>
   );
 }
