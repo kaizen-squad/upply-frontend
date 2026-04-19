9ce2e2b4958e240f871e167104e8f7457cc3b26a
DevLawer <seathieldev@gmail.com>
2026-04-19 02:09:03 +0200

    Added the sidebar component

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,12 @@
 'use client'
 import Tab from '@/components/ui/Tabs/Tab'
 import Image from 'next/image'
-import { redirect, usePathname } from 'next/navigation'
+import { redirect, usePathname, useRouter } from 'next/navigation'
 import { FC, ReactNode } from 'react'
 
 const layout: FC<{children:ReactNode}> = ({children}) => {
   const pathname = usePathname();
+  const router = useRouter();
   return (
     <div className='flex'>
       
@@ -24,7 +25,7 @@ const layout: FC<{children:ReactNode}> = ({children}) => {
       {/* Forms Zone */}
       <div className='w-[35%] m-auto flex flex-col'>
         <Tab options={['Login', 'Register']} current={pathname?.includes('login') ? 'Login' : 'Register'} onclick={(e)=> {
-            redirect((e.currentTarget.id  === 'Login' ? '/login' : '/register'))
+            router.push((e.currentTarget.id  === 'Login') ? '/login' : '/register')
         }} />
         <div className='h-max m-auto w-full'>
           {children}
