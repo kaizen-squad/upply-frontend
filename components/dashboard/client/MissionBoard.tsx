eec5abbc6c45f6edf9c60ceef55bcf50ecfa36b9
DevLawer <seathieldev@gmail.com>
2026-04-25 13:02:58 +0200

    Finished the client dashboard main page

--- components/dashboard/client/MissionBoard.tsx
+++ components/dashboard/client/MissionBoard.tsx
@@ -1,14 +1,28 @@
+'use client'
 import { flagColor } from '@/components/tasks/Task'
+import Button from '@/components/ui/Button/Button'
 import { TaskProps } from '@/types'
+import { ArrowRight, Loader } from 'lucide-react'
 import React from 'react'
 
-const MissionBoard:React.FC<{tasks: TaskProps[]}> = ({tasks}) => {
+const MissionBoard:React.FC<{tasks: TaskProps[], refetch: (id:string)=>void}> = ({tasks, refetch}) => {
 
   return (
     <div>
         {
             Boolean(tasks.length) && 
             <div>
+                <div className='flex items-center justify-between'>
+                    <h2 className='my-3'>MISSIONS RECENTES</h2>
+                    <button 
+                        className='flex bg-alizarin-crimson-red-51 rounded-md text-white-solid font-semibold text-xl items-center px-4 py-2 gap-3 scale-80 cursor-pointer duration-200 hover:opacity-80 hover:scale-77'
+                        onClick={()=>refetch('mine')}
+                    >
+                        <span className=''>Refresh</span>
+                        <Loader width={19} strokeWidth={3} />
+                    </button>
+                </div>
+
                 <table className='mt-10 w-full shadow-2xs border border-gray-200 hidden lg:block'>
                     <thead className='text-left text-xs bg-gallery-gray-93'>
                         <tr>
@@ -21,9 +35,9 @@ const MissionBoard:React.FC<{tasks: TaskProps[]}> = ({tasks}) => {
 
                     <tbody>
                         {tasks.map(({title, deadline, budget, status})=>(
-                            <tr className='border-b border-b-gray-300'>
+                            <tr key={title} className='border-b border-b-gray-300'>
                                 <td className='px-8 w-[45%] h-max py-6 font-semibold'>
-                                    <p className='border-l-4 pl-3' h-max style={{borderColor: `var(--${flagColor[status]})`}}>{title}</p>
+                                    <p className='border-l-4 pl-3 h-max' style={{borderColor: `var(--${flagColor[status]})`}}>{title}</p>
                                 </td>
                                 <td className='text-jumbo-gray-46 px-8'>{deadline}</td>
                                 <td className='px-8'>{budget}</td>
@@ -37,7 +51,7 @@ const MissionBoard:React.FC<{tasks: TaskProps[]}> = ({tasks}) => {
                 
                 <div className='block md:grid md:grid-cols-2 md:gap-7 lg:hidden mt-10'>
                     {tasks.map(({title, deadline, budget, status})=>(
-                        <div className='my-5 md:my-0 bg-white-solid p-5 rounded-md shadow-xl'>
+                        <div key={title} className='my-5 md:my-0 bg-white-solid p-5 rounded-md shadow-xl'>
                             <div className='flex items-center justify-between my-2'>
                                 <p className='font-semibold w-[70%] line-clamp-1' title={title}>{title}</p>
                                 <p className='text-alizarin-crimson-red-51 font-semibold line-clamp-1 text-lg'>{budget} <small>FCFA</small> </p>
@@ -56,9 +70,18 @@ const MissionBoard:React.FC<{tasks: TaskProps[]}> = ({tasks}) => {
         }
         {
             !Boolean(tasks.length) && 
-            <div className='w-max m-auto mt-20'>
-                {/* Etat vide en attente de design */}
-                <h2>Créez une mission pour en suivre l'activité.</h2>
+            <div className='w-full m-auto mt-20 text-center bg-white-solid border border-gray-300'>
+                <div className='w-[70%] m-auto py-15'>
+                    <h2 className='my-3'>Lancer un projet maintenant</h2>
+                    <p className='my-3 text-santa-gray'>Décrivez votre besoin et nous trouverons le prestataire idéal pour votre mission.</p>
+                    <Button
+                        textContent='COMMENCER ICI'
+                        Icon={ArrowRight}
+                        Iposition='right'
+                        className='bg-alizarin-crimson-red-51 rounded-sm py-3 px-5 text-white w-max m-auto mt-5'
+
+                    />
+                </div>
             </div>
         }
     </div>
