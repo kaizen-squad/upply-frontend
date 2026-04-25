11d4d05c7748d713a0788d25519373eaa0cc9cd6
DevLawer <seathieldev@gmail.com>
2026-04-25 10:44:33 +0200

    Added the new task page with submit logic

---
+++ app/(client)/client/dashboard/tasks/new/page.tsx
@@ -0,0 +1,17 @@
+import TaskForm from '@/components/dashboard/client/TaskForm'
+
+const page = () => {
+  return (
+    <div className='pl-15'>
+        <div className='my-5 mb-8'>
+            <h1>Publier une nouvelle mission</h1>
+            <p className='text-santa-gray my-2'>Mettez vos besoins entre les mains de prestataires certifiés.</p>
+        </div>
+        <div>
+            <TaskForm/>
+        </div>
+    </div>
+  )
+}
+
+export default page
