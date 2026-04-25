dbba5c0fb090fd0e1be5faf81471c1d6db6a2693
DevLawer <seathieldev@gmail.com>
2026-04-25 13:02:58 +0200

    Finished the client dashboard main page

--- app/(client)/client/dashboard/page.tsx
+++ app/(client)/client/dashboard/page.tsx
@@ -5,12 +5,11 @@ import Button from '@/components/ui/Button/Button';
 import Spinner from '@/components/ui/Spinner/Spinner';
 import { useTasks } from '@/hooks/useTasks';
 import { TaskProps } from '@/types';
-import { Loader, Plus, RotateCw, Zap } from 'lucide-react';
+import { BookOpenCheck, Lightbulb, Loader, Plus, Zap } from 'lucide-react';
 
 const page = () => {
     const {tasks, loading, refetch} = useTasks('mine');
-
- const mockTasks: TaskProps[] = [
+ const mockTasks: TaskProps[] =  [
   {
     id: "task_001",
     client_id: "client_001",
@@ -113,28 +112,50 @@ const page = () => {
   }
 ];
     const formatNumber = (number:number) => {
-        return (number < 10 && number!== 0) ? `0${number}` : number;
+        return number < 10 ? `0${number}` : number;
     }
   return (
-    <div className='block grid-cols-[67%_3%_30%] m-auto w-[90%] lg:w-full lg:pl-10 xl:grid'>
+    
+    <div className='block grid-cols-[67%_3%_30%] m-auto w-[90%] lg:w-[95%] lg:m-auto xl:grid'>
         <div>
             <div className='md:flex items-center justify-between'>
                 <div>
-                    <h1>VUE D'ENSEMBLE</h1>
-                    <p className='text-santa-gray text-[0.9rem] mt-2 lg:text-md'>Suivi en temps réel de votre activité opérationnelle.</p>
+                    {
+                        loading ? 
+                        <div>
+                            <p className='h-10 rounded-md bg-gray-200 w-50 animate-pulse lg:mt-3'></p> 
+                            <p  className='h-7 rounded-md bg-gray-200 w-80 animate-pulse lg:mt-3'></p>
+                        </div>
+                            
+                        : (
+                            Boolean(tasks.length) ? 
+                            <div>
+                                <h1>VUE D'ENSEMBLE</h1>
+                                <p className='text-santa-gray text-[0.9rem] mt-2 lg:text-md'>Suivi en temps réel de votre activité opérationnelle.</p>
+                            </div>  : 
+                            <div>
+                                <h1>Prêt à déléguer votre première tâche ?</h1>
+                                <p className='text-santa-gray text-[0.9rem] mt-2 lg:text-md'>Votre tableau de bord est prêt à recevoir vos projets.</p>
+                            </div>
+                        )
+                        }
+                    
                 </div>
 
-                <Button 
-                    textContent='NOUVELLE MISSION'
-                    Icon={Plus}
-                    className='min-w-max px-5 py-3 rounded-sm bg-alizarin-crimson-red-51 font-medium text-white-solid scale-80 hover:scale-77 duration-200 hidden md:flex'
-                />          
+                {
+                    (!loading && Boolean(tasks.length)) &&
+                    <Button 
+                        textContent='NOUVELLE MISSION'
+                        Icon={Plus}
+                        className='min-w-max px-5 py-3 rounded-sm bg-alizarin-crimson-red-51 font-medium text-white-solid scale-80 hover:scale-77 duration-200 hidden md:flex'
+                    />
+                }          
             </div>
 
             <div className='lg:grid-cols-3 lg:gap-8 gap-5 mt-10 grid'>
                 <div 
-                className='border-l-7 flex items-center justify-between lg:block rounded-sm bg-white-solid p-5 lg:border-2 lg:border-l-7 border-gray-200 shadow-2xs' 
-                style={{borderLeftColor: `var(--${flagColor.OUVERTE})`}}>
+                className='border-l-7 border-l-scorpion-gray-37 flex items-center justify-between lg:block rounded-sm bg-white-solid p-5 lg:border-2 lg:border-gray-200 shadow-2xs' 
+                >
                     <small className='text-jumbo-gray-46 font-semibold'>MISSIONS OUVERTES</small>
                     {
                         !loading ? 
@@ -144,7 +165,7 @@ const page = () => {
                     }
                     
                 </div>
-                <div className='border-l-7 flex items-center justify-between lg:block rounded-sm bg-white-solid p-5 lg:border-2 border-gray-200 shadow-2xs lg:border-l-7' style={{borderLeftColor: `var(--${flagColor.EN_COURS})`}}>
+                <div className='border-l-7 border-l-orange-alert flex items-center justify-between lg:block rounded-sm bg-white-solid p-5 lg:border-2 lg:border-gray-200 shadow-2xs'>
                     <small className='text-jumbo-gray-46 font-semibold' >EN COURS</small>
                     {
                         !loading ? 
@@ -153,7 +174,7 @@ const page = () => {
                             <p className='h-10 rounded-md bg-gray-200 w-25 animate-pulse lg:mt-3'></p> 
                     }
                 </div>
-                <div className='border-l-7 flex items-center justify-between lg:block rounded-sm bg-white-solid p-5 lg:border-2 border-gray-200 shadow-2xs lg:border-l-7' style={{borderLeftColor: `var(--${flagColor.VALIDEE})`}}>
+                <div className='border-l-7 border-l-green-success flex items-center justify-between lg:block rounded-sm bg-white-solid p-5 lg:border-2 lg:border-gray-200 shadow-2xs'>
                     <small className='text-jumbo-gray-46 font-semibold' >TERMINEES</small>
                     {
                         !loading ? 
@@ -165,22 +186,13 @@ const page = () => {
             </div>
 
             <div className='mt-15'>
-                <div className='flex items-center justify-between'>
-                    <h2 className='my-3'>MISSIONS RECENTES</h2>
-                    <button 
-                        className='flex bg-alizarin-crimson-red-51 rounded-md text-white-solid font-semibold text-xl items-center px-4 py-2 gap-3 scale-80 cursor-pointer duration-200 hover:opacity-80 hover:scale-77'
-                        onClick={()=>refetch('mine')}
-                    >
-                        <span className=''>Refresh</span>
-                        <Loader width={19} strokeWidth={3} />
-                    </button>
-                </div>
+                
                 {
                     !loading ?
-                        <MissionBoard tasks={mockTasks}/>
+                        <MissionBoard tasks={mockTasks} refetch={refetch} />
                     :
                         <div className='flex items-center gap-5 w-max m-auto mt-10'>
-                            {/* En attendant l'etat de chartgement designé */}
+                            {/* En attendant l'etat de chargement designé */}
                             <Spinner/>
                             <small className='text-lg font-semibold'>Loading tasks...</small>
                         </div>
@@ -189,15 +201,76 @@ const page = () => {
             </div>
         </div>
         <div></div>
-        <div className='px-5 py-8 bg-woodsmoke-gray-8 rounded-sm h-max md:max-w-[85%] xl:w-full md:w-max mt-10 mx-auto'>
-            <Zap stroke='white' strokeWidth={2}/>
-            <h3 className='my-2 text-white-solid'>Accélerez vos missions</h3>
-            <p className='text-santa-gray'>Boostez la visibilité de vos taches pour un recrutement 2x plus rapide.</p>  
-            <Button
-                textContent='EN SAVOIR PLUS'
-                className='text-xs text-alizarin-crimson-red-51 mt-2'
-            />
+
+        <div>
+            {
+                Boolean(tasks.length) &&
+               <div className='grid grid-cols-2 gap-5 lg:block'>
+                    <div className='px-5 py-8 bg-woodsmoke-gray-8 rounded-sm h-max md:max-w-[85%] xl:w-full md:w-max mt-10 mx-auto'>
+                        <Zap stroke='white' strokeWidth={2}/>
+                        <h3 className='my-2 text-white-solid'>Optimisez votre annonce</h3>
+                        <p className='text-santa-gray'>Une description détaillée avec des étapes claires augmente vos chances de recevoir des candidatures qualifiées de 40%. N'hésitez pas à joindre des fichiers de référence pour guider les prestataires.</p>  
+                    </div>
+
+                    <div className='px-5 py-8 bg-woodsmoke-gray-8 rounded-sm h-max md:max-w-[85%] xl:w-full md:w-max mt-10 mx-auto'>
+                        <Zap stroke='white' strokeWidth={2}/>
+                        <h3 className='my-2 text-white-solid'>Notez vos prestaires</h3>
+                        <p className='text-santa-gray'>Évaluez vos prestataires avec justesse. Un retour constructif après chaque mission aide la communauté à identifier les meilleurs talents et améliore la qualité globale des livrables sur Upply.</p>  
+                    </div>
+
+                    <div className='px-5 py-8 bg-woodsmoke-gray-8 rounded-sm h-max md:max-w-[85%] xl:w-full md:w-max mt-10 mx-auto'>
+                        <div className='flex items-center gap-2'>
+                            <Lightbulb className='text-orange-500'/>
+                            <h3 className='my-2 text-white-solid'>Optimisez votre annonce</h3>
+                        </div>
+                        <p className='text-santa-gray'>Comparez les notes et les expériences passées des candidats. Un candidat avec une expérience spécifique dans l'e-commerce garantira souvent de meilleurs résultats pour cette mission..</p>  
+                    </div>
+                </div> 
+            }
+            
+            {
+                (!loading && !Boolean(tasks.length)) &&
+                <div className='flex h-full mt-10 xl:m-0'>
+                    <div className='h-max m-auto'>
+                        <div className='bg-white-solid border-2 border-gray-300'>
+                            <div className='flex items-center gap-3 py-5 px-5'>
+                                <BookOpenCheck width={30} className='text-orange-500'/>
+                                <h2>Guide de démarrage</h2>
+                            </div>
+                            <hr className='w-full border-gray-200' />
+
+                            <div className='px-5 py-5'>
+                                {
+                                    [
+                                        {textLeft:'COMPTE', textRight:"100'", bg:'#E31A1C', color:''},
+                                        {textLeft:'PROFIL', textRight:"80'", bg:'linear-gradient(to right, #1A1A1A 80%, #EEE 20%)', color:''},
+                                        {textLeft:'MISSION', textRight:"0'", bg:'#EEE', color: 'var(--alizarin-crimson-red-51)'}
+                                    ].map(({textLeft, textRight, bg, color}, index)=> <div key={`${textLeft}-${index}`} className='my-3'>
+                                        <div className='flex items-center justify-between mb-1' style={{color:color}}>
+                                            <small className='font-semibold'>{textLeft}</small>
+                                            <p className='text-santa-gray' style={{color:color}}>{textRight}</p>
+                                        </div>
+                                        <div className='h-1.5 w-full border-2 border-gray-300' style={{background: bg}}></div>
+                                    </div>)
+                                }
+                                
+                                
+                            </div>
+                        </div>
+
+                        <div className='bg-white-solid border-2 border-gray-300 mt-5'>
+                            <div className='flex items-center gap-2 p-5 '>
+                                <Lightbulb className='text-orange-500'/>
+                                <h3 className='my-2'>Le saviez-vous?</h3>
+                            </div>
+                            <p className='p-5 bg-gallery-gray-93'>Upply assure une transparence totale de vos transactions. Chaque étape de la facturation est sécurisée garantissant une fiabilité à 100%</p>
+                        </div>
+                      
+                    </div>
+                </div>
+            }
         </div>
+        
             
     </div>
   )
--- components/ui/Button/Button.tsx
+++ components/ui/Button/Button.tsx
@@ -3,7 +3,7 @@ import type { FC } from 'react';
 import type { IButtonProps } from '../types';
 import { cn } from '@/lib/utils';
 
-const Button: FC<IButtonProps> = ({type, textContent, className, Icon, onClick, ...properties})=>{
+const Button: FC<IButtonProps> = ({type, textContent, className, Icon, Iposition='left', onClick, ...properties})=>{
 
     return (
         <button 
@@ -14,12 +14,16 @@ const Button: FC<IButtonProps> = ({type, textContent, className, Icon, onClick, 
                     onClick(e)
             }}
             className={cn(
+                Iposition === 'left' 
+                ? '' : 'flex-row-reverse',
                 'font-medium flex gap-3 justify-center cursor-pointer shadow-2xl',     
                (!className?.includes('hover') &&  ' hover:opacity-90 hover:scale-98 duration-200'),
                className 
             )}            
-        >
-              {Icon && ((typeof Icon === 'string') ? <img src={Icon} alt="image-logo" /> : <Icon/>)}  {textContent}
+        >  
+            {Icon && ((typeof Icon === 'string') ? <img src={Icon} alt="image-logo" /> : <Icon/>)}  
+            {textContent}
+              
         </button>
     )
 }
