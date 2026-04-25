11a119c6a97d9a370ef502585bf84f3907aa87da
DevLawer <seathieldev@gmail.com>
2026-04-25 14:18:35 +0200

    Added the header component

--- app/(client)/client/layout.tsx
+++ app/(client)/client/layout.tsx
@@ -1,14 +1,15 @@
 'use client'
 import Footer from '@/components/ui/Footer/Footer';
 import FooterMobile from '@/components/ui/Footer/FooterMobile';
+import Header from '@/components/ui/Header/Header';
 import Sidebar from '@/components/ui/Sidebar/Sidebar';
-import { TaskProps } from '@/types';
 import { ReactNode } from 'react';
 
 const layout:React.FC<{children:ReactNode}> = ({children}) => {
 
   return (
     <div>
+        <Header/>
         <div className="flex h-screen overflow-y-hidden">
             <div className="hidden md:block">
                 <Sidebar/>
