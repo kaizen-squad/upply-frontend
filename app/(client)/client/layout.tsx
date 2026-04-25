bbd3c2145dc7f94993d1eac5e91b47b606327072
DevLawer <seathieldev@gmail.com>
2026-04-25 17:25:28 +0200

    Added the sidebar client for desktop and mobile with their state

--- app/(client)/client/layout.tsx
+++ app/(client)/client/layout.tsx
@@ -1,20 +1,29 @@
 'use client'
+import SidebarClient from '@/components/dashboard/client/SidebarClient';
+import SiderbarClientMobile from '@/components/dashboard/client/SiderbarClientMobile';
 import Footer from '@/components/ui/Footer/Footer';
 import FooterMobile from '@/components/ui/Footer/FooterMobile';
 import Header from '@/components/ui/Header/Header';
-import Sidebar from '@/components/ui/Sidebar/Sidebar';
-import { ReactNode } from 'react';
+import { Overlay } from '@/components/ui/Overlay/Overlay';
+import { ReactNode, useState } from 'react';
 
 const layout:React.FC<{children:ReactNode}> = ({children}) => {
+    const [isMobileSidebarOpened, setIsMobileSidebarOpened] = useState(false);
 
   return (
     <div>
-        <Header/>
-        <div className="flex h-screen overflow-y-hidden">
-            <div className="hidden md:block">
-                <Sidebar/>
+        <Header isMobileSidebarOpened={isMobileSidebarOpened} setIsMobileSidebarOpened={setIsMobileSidebarOpened} />
+        <div className="flex h-(--main-height) overflow-y-hidden">
+            <div className=" hidden md:block">
+                <SidebarClient/>
             </div>
-            <div className="pt-15 w-full overflow-y-scroll bg-alabaster-gray-98">
+            <div className='md:hidden'>
+                {isMobileSidebarOpened && 
+                <Overlay isOpen={isMobileSidebarOpened} onClose={()=>{}}>
+                    <SiderbarClientMobile isMobileSidebarOpened={isMobileSidebarOpened} setIsMobileSidebarOpened={setIsMobileSidebarOpened}/>
+                </Overlay> }
+            </div>
+            <div className="pt-5 w-full overflow-y-scroll bg-alabaster-gray-98 no-scrollbar">
                 <div className="pb-10">
                     {children}
                 </div>
