edf0b0ddd0ba053d9e838c066b29bfeb54192548
DevLawer <seathieldev@gmail.com>
2026-04-26 21:17:59 +0200

    Render dashboard for both roles

--- app/(prestataire)/prestataire/layout.tsx
+++ app/(prestataire)/prestataire/layout.tsx
@@ -1,6 +1,6 @@
 'use client'
-import SidebarClient from '@/components/dashboard/client/SidebarClient';
-import SiderbarClientMobile from '@/components/dashboard/client/SidebarClientMobile';
+import SidebarPrestataire from '@/components/dashboard/prestataire/SidebarPrestataire';
+import SidebarPrestataireMobile from '@/components/dashboard/prestataire/SidebarPrestataireMobile';
 import Footer from '@/components/ui/Footer/Footer';
 import FooterMobile from '@/components/ui/Footer/FooterMobile';
 import Header from '@/components/ui/Header/Header';
@@ -15,16 +15,16 @@ const layout:React.FC<{children:ReactNode}> = ({children}) => {
         <Header isMobileSidebarOpened={isMobileSidebarOpened} setIsMobileSidebarOpened={setIsMobileSidebarOpened} />
         <div className="flex h-(--main-height) overflow-y-hidden">
             <div className=" hidden md:block">
-                <SidebarClient/>
+                <SidebarPrestataire/>
             </div>
             <div className='md:hidden'>
                 {isMobileSidebarOpened && 
                 <Overlay isOpen={isMobileSidebarOpened} onClose={()=>{}}>
-                    <SiderbarClientMobile isMobileSidebarOpened={isMobileSidebarOpened} setIsMobileSidebarOpened={setIsMobileSidebarOpened}/>
+                    <SidebarPrestataireMobile isMobileSidebarOpened={isMobileSidebarOpened} setIsMobileSidebarOpened={setIsMobileSidebarOpened}/>
                 </Overlay> }
             </div>
             <div className="pt-5 w-full overflow-y-scroll bg-alabaster-gray-98 no-scrollbar">
-                <div className="pb-10">
+                <div className="pb-10 px-10">
                     {children}
                 </div>
             </div>
